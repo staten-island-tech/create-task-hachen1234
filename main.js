735d1fe2ba851a71853e105e65e89ec005cc347e
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2024-01-12 10:15:06 -0500

    1

--- main.js
+++ main.js
@@ -2,7 +2,14 @@ const dom = {
   roll: document.querySelector(".roll"),
 };
 const guess = prompt("enter number 1-6 ");
+const diceresult = Math.floor(Math.random() * 6) + 1;
+console.log(diceresult);
 dom.roll.addEventListener("click", function () {
-  diceresult = Math.floor(Math.random() * 6) + 1;
-  console.log(diceresult);
+  if (guess === diceresult) {
+    document.getElementsByClassName("text").textContent = "your right";
+  } else if (guess > diceresult) {
+    document.getElementsByClassName("text").textContent = "you rolled higher";
+  } else if (guess < diceresult) {
+    document.getElementsByClassName("text").textContent = "you rolled lower";
+  }
 });
