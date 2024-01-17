52180595f16e01d2302d1e246b39e273880f6b1a
hachen1234 <90781150+hachen1234@users.noreply.github.com>
2024-01-17 10:17:09 -0500

    1

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const dom = {
   h1: document.querySelector(".text"),
   input: document.querySelector(".yournumber"),
   output: document.querySelector(".thedicenumber"),
+  historylist: document.querySelector(".history"),
 };
+const history = [];
 dom.roll.addEventListener("click", function () {
   let guess = prompt("enter number 1-6 ");
   dom.input.innerHTML = guess;
@@ -11,9 +13,13 @@ dom.roll.addEventListener("click", function () {
   dom.output.innerHTML = diceresult;
   if (guess == diceresult) {
     dom.h1.innerHTML = "your right";
+    history.push("your right");
   } else if (guess > diceresult) {
     dom.h1.innerHTML = "you guessed higher";
+    history.push("you guessed higher");
   } else if (guess < diceresult) {
     dom.h1.innerHTML = "you guessed lower";
+    history.push("you guessed lower");
   }
 });
+console.log(history);
